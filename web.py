81e67e66704b8ee3d62d539b636d7f8cd8eef39a
criticalmiind <70806284+shawalahmad123@users.noreply.github.com>
2021-08-07 17:33:08 +0500

    changes done

--- web.py
+++ web.py
@@ -1,12 +1,15 @@
-from http_request_randomizer.requests.proxy.requestProxy import RequestProxy
+import os
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
 import time
 import random
 import urllib.request , socket
+# from seleniumwire import webdriver  # Import from seleniumwire
 
 chrome_driver_path = ChromeDriverManager().install()
 socket.setdefaulttimeout(180)
+stay_on_page = 15
+pass_random_urls = False
 
 def is_bad_proxy(pip):    
     try:        
@@ -36,6 +39,13 @@ def read_urls_from_file(file='urls.txt'):
         urls = f.readlines()
     return urls
 
+# Create a request interceptor
+def interceptor(request):
+    del request.headers['User-Agent']  # Delete the header first
+    request.headers['User-Agent'] = 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1'
+    del request.headers['referer']  # Delete the header first
+    request.headers['referer'] = 'https://www.google.com'
+
 proxies = read_proxies_from_file()
 if len(proxies) < 1 :
     print("No proxy found")
@@ -46,17 +56,20 @@ if len(urls) < 1 :
     print("No url found")
     exit()
 
-for p in proxies:
-    url = random.choice(urls)
+stay = input("Enter delay time(seconds) on page:")
+if stay: stay_on_page = stay
+os.system('cls||clear')
+print("Default delay time set:", stay_on_page, "seconds... \n")
 
-    print("Check your proxy ip:",p)
-    time.sleep(1)
+for p in proxies:
+    print("Checking proxy server:",p)
     if is_bad_proxy(p):
-        # print("Bad proxy ip and port :",p,"\n\n")
+        os.system('cls||clear')
+        print("Bad proxy ip and port :",p)
         continue
     else:
-        print("Valid proxy...\n","Opeing chrome...\n","Link:",url)
-        time.sleep(1)
+        os.system('cls||clear')
+        print("Valid proxy...","\nOpeing chrome...","\nProxy:",p)
     
 
     try:
@@ -75,19 +88,29 @@ for p in proxies:
         options = webdriver.chrome.options.Options()
         options.add_argument(chrome_driver_path)
         options.add_argument("--disable-extensions") # optional and off-topic, but it conveniently prevents the popup 'Disable developer mode extensions' 
-        # browser = webdriver.Chrome(chrome_options=options)
         # options.add_argument('--proxy-server=%s' % p)
+        
+        # Set the interceptor on the driver
+        # browser.request_interceptor = interceptor
 
-        # browser = webdriver.Chrome(options=options)
-
-        browser = webdriver.Chrome(chrome_driver_path, options=options)
-        browser.get(url)
-        # browser.get("https://api.scrapingdog.com/scrape?api_key=610b78ecd667b826c9b6816e&url=https://www.youtube.com/watch?v=Gq0YD1E8f70")
-        time.sleep(15)
-        browser.stop_client()
-        browser.close()
-        browser.quit()
+        for u in urls:
+            url = u
+            try:
+                browser = webdriver.Chrome(chrome_driver_path, options=options)
+                if pass_random_urls: url = random.choice(urls)
+                print("Opening url:",url)
+                browser.get(url)
+                time.sleep(int(stay_on_page))
+                browser.stop_client()
+                browser.close()
+                browser.quit()
+            except Exception as a:
+                options = webdriver.ChromeOptions()
+                options.add_experimental_option("detach", True)
+                driver = webdriver.Chrome(chrome_driver_path, chrome_options=options)
+                print("\n\n Error 001???", a)
+                break
     except:
-        print("\n\n Error???")
+        print("\n\n Error 002???")
         raise
 
